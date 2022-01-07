f50f4b7f0382eab481a0bc1d03fa85cf0d0c2639
TanyaDanilovich <tan.danilovich@gmail.com>
2022-01-07 20:59:58 +0300

    feat: task is solv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = '';
+    for (let i = n.length - 1; i >= 0; i--) {
+        str = str + n[i]
+    }
+    return +str;
 }
