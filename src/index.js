2c9bd1844ee3a57a7f3e55892a0b8f47dd5383db
TanyaDanilovich <tan.danilovich@gmail.com>
2022-01-10 09:08:03 +0300

    fix: task solution fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 module.exports = function reverse(n) {
-    let str = '';
-    for (let i = n.length - 1; i >= 0; i--) {
-        str = str + n[i]
-    }
-    return +str;
+	let str = '';
+	n = String(n);
+	if (n[0] != '-') {
+		for (let i = n.length - 1; i >= 0; i--) {
+			str = str + n[i]
+		}
+	} else {
+		//str = '-'
+		for (let i = n.length - 1; i > 0; i--) {
+			str = str + n[i]
+		}
+	}
+	return Number(str);
 }
